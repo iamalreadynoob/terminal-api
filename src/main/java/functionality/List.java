693dea178f86a505b8eba7c611a82ab7d0c4e9db
iamalreadynoob <22120205381@ismu.edu.tr>
2023-10-18 15:20:48 +0300

    commit

--- src/main/java/functionality/List.java
+++ src/main/java/functionality/List.java
@@ -2,6 +2,8 @@ package functionality;
 
 import fileWorks.SAVF;
 
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.ArrayList;
 
 public class List
@@ -13,13 +15,18 @@ public class List
 
     public List init()
     {
-        SAVF savf = new SAVF("config.savf");
-        savf.scan();
+        if (words.size() == 1) return new List(words);
+        else
+        {
+            SAVF savf = new SAVF("config.savf");
+            savf.scan();
 
-        String raw = savf.getValue("list-commands");
-        String[] pieces = raw.split(",");
+            String raw = savf.getValue("list-commands");
+            String[] pieces = raw.split(",");
 
-        for (String p: pieces) if (words.get(1).equals(p)) return new List(words);
+            for (String p: pieces) if (words.get(1).equals(p)) return new List(words);
+        }
+
         return new List(null);
     }
 
@@ -28,11 +35,134 @@ public class List
         if (words != null)
         {
             String result = null;
-            
+
+            if (words.size() == 1) result = list();
+            else if (words.get(1).equals("at")) result = at();
+            else if (words.get(1).equals("which")) result = which();
+            else if (words.get(1).equals("ends")) result = ends();
+            else if (words.get(1).equals("starts")) result = starts();
 
             return result;
         }
         else return TerminalExceptions.getException(TerminalExceptions.Failures.INVALID_SUB_COMMAND);
+    }
+
+
+    private String list()
+    {
+        String result = null;
+
+        SAVF savf = new SAVF("config.savf");
+        savf.scan();
+
+        Path path = Paths.get(savf.getValue("bubble-loc"));
+
+        try(DirectoryStream<Path> stream = Files.newDirectoryStream(path))
+        {
+            for (Path file: stream)
+            {
+                if (result == null) result = file.getFileName().toString();
+                else result += "\n" + file.getFileName().toString();
+            }
+        }
+        catch (IOException | DirectoryIteratorException e) {e.printStackTrace();}
+
+        return result;
+    }
+
+    private String at()
+    {
+        String result = null;
+
+        if (words.size() == 3)
+        {
+            SAVF savf = new SAVF("config.savf");
+            savf.scan();
+
+            Path path = Paths.get(savf.getValue("bubble-loc") + "/" + words.get(2));
+
+            try(DirectoryStream<Path> stream = Files.newDirectoryStream(path))
+            {
+                for (Path file: stream)
+                {
+                    if (result == null) result = file.getFileName().toString();
+                    else result += "\n" + file.getFileName().toString();
+                }
+            }
+            catch (IOException | DirectoryIteratorException e) {result = e.getMessage();}
+        }
+        else result = TerminalExceptions.getException(TerminalExceptions.Failures.NO_PLACE);
+
+        return result;
+    }
+
+    private String which()
+    {
+        String result = null;
+
+
+
+        return result;
+    }
+
+    private String ends()
+    {
+        String result = null;
+
+        if (words.size() == 3)
+        {
+            SAVF savf = new SAVF("config.savf");
+            savf.scan();
+
+            Path path = Paths.get(savf.getValue("bubble-loc") + "/" + words.get(2));
+
+            try(DirectoryStream<Path> stream = Files.newDirectoryStream(path))
+            {
+                ArrayList<String> items = new ArrayList<>();
+                for (Path file: stream) items.add(file.getFileName().toString());
+                for (String i: items)
+                    if (i.endsWith(words.get(2)))
+                    {
+                        if (result == null) result = i;
+                        else result += "\n" + i;
+                    }
+
+                if (result == null) result = "";
+            }
+            catch (IOException | DirectoryIteratorException e) {result = e.getMessage();}
+        }
+
+        return result;
+    }
+
+    private String starts()
+    {
+        String result = null;
+
+        if (words.size() == 3)
+        {
+            SAVF savf = new SAVF("config.savf");
+            savf.scan();
+
+            Path path = Paths.get(savf.getValue("bubble-loc") + "/" + words.get(2));
+
+            try(DirectoryStream<Path> stream = Files.newDirectoryStream(path))
+            {
+                ArrayList<String> items = new ArrayList<>();
+                for (Path file: stream) items.add(file.getFileName().toString());
+                for (String i: items)
+                    if (i.startsWith(words.get(2)))
+                    {
+                        if (result == null) result = i;
+                        else result += "\n" + i;
+                    }
+
+                if (result == null) result = "";
+            }
+            catch (IOException | DirectoryIteratorException e) {result = e.getMessage();}
+        }
+
+        return result;
     }
 
 }
--- src/main/java/functionality/TerminalExceptions.java
+++ src/main/java/functionality/TerminalExceptions.java
@@ -7,12 +7,13 @@ public class TerminalExceptions
     {
         if (failure == Failures.INVALID_INIT_COMMAND) return "INVALID INIT COMMAND: The main action is not defined.";
         else if (failure == Failures.INVALID_SUB_COMMAND) return "INVALID SUB COMMAND: The main action is defined but sub functionality is not defined.";
+        else if (failure == Failures.NO_PLACE) return "NO PLACE: The place as pointed in path is not exist.";
         else return "UNEXPECTED FAILURE: There is no trace for its reason.";
     }
 
     public enum Failures
     {
-        INVALID_INIT_COMMAND, INVALID_SUB_COMMAND
+        INVALID_INIT_COMMAND, INVALID_SUB_COMMAND, NO_PLACE
     }
 
 }
